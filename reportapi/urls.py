0269cd39969874f3108be9bdf259590a1c61f618
Gustavo Aires <gustavoairesmatos@gmail.com>
2019-09-12 20:30:20 -0300

    removing admin route

--- reportapi/urls.py
+++ reportapi/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^reports/', include('reports.api.urls'), name='report-api'),
 ]
